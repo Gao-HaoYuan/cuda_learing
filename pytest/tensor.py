c3a51f8609179dcd1db57b2320a767f6162158bf
gaohaoyuan <ghy_mike@163.com>
2025-06-27 19:55:48 +0800

    update pytest

--- pytest/tensor.py
+++ pytest/tensor.py
@@ -6,12 +6,19 @@ import torch.nn as nn
 
 from pytorch_profiler import perf_kernel as perf
 
-device = torch.device('cuda')
-linear = nn.Linear(100, 128).to(device)
+device = torch.device('cuda:0')
+linear1 = nn.Linear(100, 128).to(device)
+linear2 = nn.Linear(100, 128).to(device)
+
 x = torch.randn(32, 100, device=device, requires_grad=True)
 
+def forward(input):
+    out1 = linear1(input)
+    out2 = linear2(input)
+    return out1 + out2
+
 perf.measure_step_memory(lambda: None, "idle step")
-y1 = perf.measure_step_memory(lambda: linear(x), desc="linear1(x)")
+y1 = perf.measure_step_memory(lambda: forward(x), desc="forward(x)")
 z = perf.measure_step_memory(lambda: y1.sum(), desc="y1.sum()")
 perf.measure_step_memory(lambda: z.backward(), desc="backward()")
 perf.measure_step_memory(lambda: None, "idle step")

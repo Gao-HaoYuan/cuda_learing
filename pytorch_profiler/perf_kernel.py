41deeba996300b7e03b7a01be22a88829cf09928
gaohaoyuan <ghy_mike@163.com>
2025-07-10 20:51:53 +0800

    update perf

--- pytorch_profiler/perf_kernel.py
+++ pytorch_profiler/perf_kernel.py
@@ -1,3 +1,4 @@
+import math
 from typing import Callable
 
 import torch
@@ -8,20 +9,18 @@ def perf_profile(operator_fn, *args, **kwargs):
     # 同步 cuda 行为, 不执行同步 cuda 统计会出错，比如内存峰值
     def sync_op():
         torch.cuda.synchronize()
-        _ = operator_fn(*args, **kwargs)
+        operator_fn(*args, **kwargs)
         torch.cuda.synchronize()
-
-        return _
 
     with torch.profiler.profile(
         activities = [ProfilerActivity.CPU, ProfilerActivity.CUDA],
-        schedule = torch.profiler.schedule(wait=1, warmup=1, active=3, repeat=1),
-        on_trace_ready = torch.profiler.tensorboard_trace_handler('./log/linear'),
+        schedule = torch.profiler.schedule(skip_first=3, wait=1, warmup=1, active=5, repeat=1),
+        on_trace_ready = torch.profiler.tensorboard_trace_handler('./log/perf'),
         record_shapes = True,
         with_stack = True,
         profile_memory = True
     ) as prof:
-        for _ in range(5):
+        for _ in range(10):
             sync_op()
             prof.step()
 
@@ -34,25 +33,31 @@ def perf_profile(operator_fn, *args, **kwargs):
     ))
 
     torch.cuda.reset_peak_memory_stats()
-    output = sync_op()
+    sync_op()
     peak_cuda_mem_MB = torch.cuda.max_memory_allocated() / (1024**2)
     print(f"Peak GPU Memory Usage: {peak_cuda_mem_MB:.3f} MB\n")
 
-    return output
 
-
-def perf_accuracy(expected, compared):
+def perf_accuracy(expected, compared, threshold=1.0):
     def compare(x, y):
+        info = ""
+        if torch.all(x == 0) and torch.all(y == 0):
+            err = torch.tensor(0.0)
+            corr = torch.tensor(1.0)
+            info = "Note: Both tensors are all zeros."
+            return err, corr, info
+
         err = torch.mean(torch.abs(x - y)) / torch.mean(torch.abs(x))
-        corr = torch.cosine_similarity(x.view(-1), y.contiguous().view(-1), dim=-1)
-        return err, corr
+        corr = torch.cosine_similarity(x.contiguous().view(-1), y.contiguous().view(-1), dim=-1)
+        return err, corr, info
 
     expected = expected if isinstance(expected, (list, tuple)) else (expected,)
     compared = compared if isinstance(compared, (list, tuple)) else (compared,)
 
     for i, (a, b) in enumerate(zip(expected, compared)):
-        err, corr = compare(a, b)
-        print(f"Output {i}: relative_error_mean {err:.6f}, corr: {corr:.6f}")
+        err, corr, info = compare(a, b)
+        print(f"Output {i}: relative_error_mean {err:.6f}, corr: {corr:.6f}. {info}")
+        assert math.isclose(corr.item(), threshold, rel_tol=1e-6)
 
 
 def measure_step_memory(fn: Callable, desc="", device_id=0):
